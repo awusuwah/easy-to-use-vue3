3f8ec3e77897797e8ca4175d208f14f4b8074823
Marko Maric <marko@markomaric.me>
2023-07-11 21:06:53 +0200

    Added card component. Added login page with the login fields.

---
+++ src/components/card/Card.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="divide-y divide-bgr-200 overflow-hidden rounded-lg bg-bgr shadow max-w-4xl">
+    <div class="px-4 py-5 sm:px-6">
+      <h3 class="text-xl leading-6 font-medium text-txt">{{ heading }}</h3>
+    </div>
+    <div class="px-4 py-5 sm:p-6 text-txt">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  // The title which is displayed in the header.
+  heading: {
+    type: String,
+    default: "",
+  },
+});
+</script>
--- src/pages/login.vue
+++ src/pages/login.vue
@@ -5,10 +5,34 @@
         <UserIcon class="w-8 h-8" />
       </template>
     </Header>
+
+    <main class="p-4">
+      <Card heading="Login">
+        <div class="space-y-2">
+          <TextInput v-model="email" type="email" label="Email Address" placeholder="marko.maric@hotelplan.com" />
+          <TextInput v-model="password" type="password" label="Password" placeholder="********" />
+
+          <Button variant="cta" @click="login">Login</Button>
+        </div>
+      </Card>
+    </main>
   </div>
 </template>
 
 <script setup lang="ts">
 import { UserIcon } from "@heroicons/vue/24/outline";
+import { Button, TextInput } from "ui-components-3";
+
+import Card from "../components/card/Card.vue";
 import Header from "../components/header/Header.vue";
+
+const email = ref("");
+const password = ref("");
+
+/**
+ * Validate the form and authenticate the user with the provided credentials.
+ */
+function login() {
+  console.info("Logging in with the following email address: ", email.value);
+}
 </script>
